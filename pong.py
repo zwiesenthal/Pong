18430db1ea898338afbcdc910267d98e0a375356
Zach Wiesenthal <zwiesenthal@gmail.com>
2021-05-16 19:42:05 -0700

    Pong Done, scoring added

--- pong.py
+++ pong.py
@@ -19,10 +19,11 @@ BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 PURPLE = (255, 0, 255)
+ORANGE = (255, 100, 100)
 MY_COLOR = (10, 230, 200)
 
 class Paddle(pygame.sprite.Sprite): #Paddle inherits from Sprite class
-    def __init__(self, startX, startY):
+    def __init__(self, startX, startY, color=MY_COLOR):
         super().__init__() #Calls Sprite class constructor
         
         self.width = 10
@@ -33,7 +34,7 @@ class Paddle(pygame.sprite.Sprite): #Paddle inherits from Sprite class
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
         
-        pygame.draw.rect(self.image, MY_COLOR, (0, 0, self.width, self.height))
+        pygame.draw.rect(self.image, color, (0, 0, self.width, self.height))
         self.rect = self.image.get_rect()
 
         self.rect.x = startX
@@ -76,7 +77,7 @@ class Ball(pygame.sprite.Sprite):
 
         # ball bounces off scoreboard and bottom
         if self.rect.y <= SCORE_HEIGHT or self.rect.y >= HEIGHT:
-            self.ySpeed = -self.ySpeed
+            self.ySpeed = -self.ySpeed            
 
     def bounce(self):
         if self.xSpeed < 0:
@@ -88,9 +89,14 @@ class Ball(pygame.sprite.Sprite):
         self.ySpeed = -self.ySpeed
         self.ySpeed += random.randint(-3, 3)
 
+    def reset(self):
+        self.rect.x = WIDTH / 2
+        self.rect.y = NEW_HEIGHT / 2
+        self.xSpeed = random.uniform(-5, 5) # float: decimal number
+        self.ySpeed = random.randint(-5, 5) # int: integer whole number
 
-player1 = Paddle(50, NEW_HEIGHT / 2)
-player2 = Paddle(WIDTH - 50, NEW_HEIGHT / 2)
+player1 = Paddle(50, NEW_HEIGHT / 2, PURPLE)
+player2 = Paddle(WIDTH - 50, NEW_HEIGHT / 2, ORANGE)
     
 ball = Ball()
 
@@ -99,6 +105,10 @@ all_sprites = pygame.sprite.Group()
 all_sprites.add(player1)
 all_sprites.add(player2)
 all_sprites.add(ball)
+
+score1 = 0
+score2 = 0
+winScore = 3# first to 3 points wins
 
 
 # clock used for frames per second
@@ -133,6 +143,21 @@ while(running):
     # Main Game Functionality
     all_sprites.update()
 
+    # Ball scores
+    if ball.rect.x >= WIDTH: # player 1 scores
+        score1 += 1
+        ball.reset()
+        if score1 > winScore:
+            print("Player 1 Wins!")
+            running = False
+        
+    elif ball.rect.x <= 0: # player 2 scores
+        score2 += 1
+        ball.reset()
+        if score2 >= winScore:
+            print("Player 2 Wins!")
+            running = False
+
     # Collision with paddle
     collision = ball.rect.collidelist([player1, player2])
     if collision != -1:
@@ -144,6 +169,13 @@ while(running):
     # draw the scoreboard outline
     pygame.draw.line(screen, WHITE, [0, SCORE_HEIGHT], [WIDTH, SCORE_HEIGHT], 4)
     pygame.draw.line(screen, WHITE, [WIDTH/2, 0], [WIDTH/2, SCORE_HEIGHT], 4)
+
+    #Draw the scores
+    font = pygame.font.Font(None, 64)
+    text1 = font.render(str(score1), 1, PURPLE)
+    screen.blit(text1, (WIDTH/4, 5))
+    text2 = font.render(str(score2), 1, ORANGE)
+    screen.blit(text2, (WIDTH*3/4, 5))
 
     all_sprites.draw(screen) # draw sprites
 
